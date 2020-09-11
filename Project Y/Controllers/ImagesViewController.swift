b1dfcb82c0c51a88b810ac0743be02a682eaf2e4
Anton Dobrynin <anton.brock1@gmail.com>
2020-09-11 17:57:50 +0300

    [Core] Немного прибран код. Остался жирный кусок, где обрабатывается коллекция

--- Project Y/Controllers/ImagesViewController.swift
+++ Project Y/Controllers/ImagesViewController.swift
@@ -12,7 +12,6 @@ class ImagesViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     var networkDataFetcher = NetworkDataFetcher()
-    var imageArr = [String]()
     
     var refreshCtrl: UIRefreshControl!
     var collectionData: [ImagesResults]!
@@ -23,46 +22,40 @@ class ImagesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        networkDataFetcher.fetchImages { (fetchResults) in
-            let results = fetchResults
-            self.collectionData = results
-            self.collectionView.reloadData()
-        }
-        
-        setupUi()
-        setupLogic()
-        
-    }
-    
-    func setupUi() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        collectionView.register(UINib(nibName: "ImagesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: Constants.imageCellIdentifier)
-    }
-    
-    func setupLogic() {
         session = URLSession.shared
         task = URLSessionDownloadTask()
         cache = NSCache()
-
         
+        getDataImages()
+        
+        setupUi()
+    }
+    
+    func setupUi() {
         self.refreshCtrl = UIRefreshControl()
         self.collectionView.refreshControl = self.refreshCtrl
 
         self.refreshCtrl.addTarget(self, action: #selector(ImagesViewController.refreshCollectionView), for: .valueChanged)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "ImagesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: Constants.imageCellIdentifier)
     }
     
     @objc func refreshCollectionView() {
         self.collectionData = []
         self.cache.removeAllObjects()
         
+        getDataImages()
+        self.collectionView.refreshControl?.endRefreshing()
+    }
+    
+    func getDataImages() {
         networkDataFetcher.fetchImages { (fetchResults) in
             let results = fetchResults
             self.collectionData = results
             self.collectionView.reloadData()
         }
-        self.collectionView.refreshControl?.endRefreshing()
     }
 }
 
@@ -82,22 +75,19 @@ extension ImagesViewController: UICollectionViewDelegate, UICollectionViewDataSo
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.imageCellIdentifier, for: indexPath) as! ImagesCollectionViewCell
         
         if collectionData != nil {
-            // 1
             if let data = collectionData {
                 let dictionary = data[(indexPath as NSIndexPath).row]
                 cell.imageView?.image = UIImage(named: "imagePlaceholder")
                 if (self.cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject) != nil) {
                     // Use cache
-                    print("Cached image used, no need to download it")
+                    print("Изображение закешировано, повторное скачивание не нужно.")
                     cell.imageView?.image = self.cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject) as? UIImage
                 } else {
                     let artworkUrl = dictionary.urls.small
                     let url:URL! = URL(string: artworkUrl)
                     task = session.downloadTask(with: url, completionHandler: { (location, response, error) -> Void in
                         if let data = try? Data(contentsOf: url){
-                            // 4
                             DispatchQueue.main.async(execute: { () -> Void in
-                                // 5
                                 // Before we assign the image, check whether the current cell is visible
                                 if let updateCell = collectionView.cellForItem(at: indexPath) as? ImagesCollectionViewCell {
                                     let img: UIImage! = UIImage(data: data)
